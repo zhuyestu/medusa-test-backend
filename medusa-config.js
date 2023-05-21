bdf1995f3509b1fe6982610b2c5526355d04585a
zhuye <zhuye256@163.com>
2023-05-22 01:24:35 +0800

    chore: 完善 Dockerfile

--- medusa-config.js
+++ medusa-config.js
@@ -25,7 +25,7 @@ try {
 const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8100,http://storefront:8100";
 
 const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
 const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
